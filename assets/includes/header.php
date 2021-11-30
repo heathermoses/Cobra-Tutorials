5fa10670380c2207a5e55a2e5fd2e7427425f966
Christopher Bo <csb3946@g.rit.edu>
2021-11-30 16:04:06 -0500

    fixed the navbar to have links to php files

--- assets/includes/header.php
+++ assets/includes/header.php
@@ -18,18 +18,18 @@
             <img src="<?php echo $page ?>assets/images/hamburger.png" alt="Cobra Tutorials Logo" class="hamburgericon">
             <!--nav-->
             <nav class="navbar">
-                <a href="overview.html">Overview of UNIX</a>
+                <a href="overview.php">Overview of UNIX</a>
                 <div class="dropdown">
                     <button class="dropbtn">Tutorials
                         <i class="fa fa-caret-down"></i>
                     </button>
                     <div class="dropdown-content">
-                        <a href="tutorial1.html">Loading UNIX</a>
-                        <a href="tutorial2.html">UNIX Structure</a>
-                        <a href="tutorial3.html">Traversing UNIX</a>
-                        <a href="tutorial4.html">Manipulating UNIX Files and Directories</a>
-                        <a href="tutorial5.html">UNIX and File Permissions</a>
-                        <a href="tutorial6.html">Helpful Commands</a>
+                        <a href="tutorial1.php">Loading UNIX</a>
+                        <a href="tutorial2.php">UNIX Structure</a>
+                        <a href="tutorial3.php">Traversing UNIX</a>
+                        <a href="tutorial4.php">Manipulating UNIX Files and Directories</a>
+                        <a href="tutorial5.php">UNIX and File Permissions</a>
+                        <a href="tutorial6.php">Helpful Commands</a>
                     </div>
                 </div>
                 <div class="dropdown">
@@ -37,15 +37,15 @@
                         <i class="fa fa-caret-down"></i>
                     </button>
                     <div class="dropdown-content">
-                        <a href="<?php echo $page ?>quizzes/quiz1.html">Loading UNIX</a>
-                        <a href="<?php echo $page ?>quizzes/quiz2.html">UNIX Structure</a>
-                        <a href="<?php echo $page ?>quizzes/quiz3.html">Traversing UNIX</a>
-                        <a href="<?php echo $page ?>quizzes/quiz4.html">Manipulating UNIX Files and Directories</a>
-                        <a href="<?php echo $page ?>quizzes/quiz5.html">UNIX and File Permissions</a>
-                        <a href="<?php echo $page ?>quizzes/quiz6.html">Helpful Commands</a>
+                        <a href="<?php echo $page ?>quizzes/quiz1.php">Loading UNIX</a>
+                        <a href="<?php echo $page ?>quizzes/quiz2.php">UNIX Structure</a>
+                        <a href="<?php echo $page ?>quizzes/quiz3.php">Traversing UNIX</a>
+                        <a href="<?php echo $page ?>quizzes/quiz4.php">Manipulating UNIX Files and Directories</a>
+                        <a href="<?php echo $page ?>quizzes/quiz5.php">UNIX and File Permissions</a>
+                        <a href="<?php echo $page ?>quizzes/quiz6.php">Helpful Commands</a>
                     </div>
                 </div>
-                <a href="cheatsheet.html">Cheat Sheet</a>
+                <a href="cheatsheet.php">Cheat Sheet</a>
                 <a href="#">Videos</a>
             </nav>
         </header> <!-- need to insert images and add paragraphs as fig captions maybe-->
