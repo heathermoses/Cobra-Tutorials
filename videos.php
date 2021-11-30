f3708476b0779ee50c38607f5573098ad77002bb
Christopher Bo <csb3946@g.rit.edu>
2021-11-30 17:06:21 -0500

    started work on the videos page. still early wip

---
+++ videos.php
@@ -0,0 +1,39 @@
+<?php
+    $page = "./";
+    include $page . "assets/includes/header.php";
+?>
+            <!-- Chris Bo/Group 3, ISTE-240, 2198-->
+            <h1 id="section4">Videos</h1>
+            <p>
+               Having trouble understanding text tutorials? Watch and follow along with our interactive tutorials! 
+            </p>
+            <div class="video-wrapper">
+                <div class="videobox">
+                    <h2>Unix's Structure</h2>
+                    <div class="iframe-container">
+                        <iframe width="650" height="360" src="https://www.youtube.com/embed/7UagLJq951s" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <div class="videobox2">
+                    <h2>Traversing Unix</h2>
+                    <div class="iframe-container">
+                        <iframe width="650" height="360" src="https://www.youtube.com/embed/XjA-Qszel_E" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <div class="videobox3">
+                    <h2>Manipulating Unix Files and Directories</h2>
+                    <div class="iframe-container">
+                        <iframe width="650" height="360" src="https://www.youtube.com/embed/XhcCMQ8hz8s" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                </div>
+                <div class="videobox4">
+                    <h2>UNIX and File Permissions</h2>
+                    <div class="iframe-container">
+                        <iframe width="650" height="360" src="https://www.youtube.com/watch?v=XhcCMQ8hz8s" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                    </div>
+                </div>
+            </div>
+
+<?php
+    include $page . "assets/includes/footer.php";
+?>
