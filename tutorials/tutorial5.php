1c0366b5b4063272d90be619196dc049114a1189
Nerdynnjah <ojo5312@g.rit.edu>
2021-12-01 18:17:35 -0500

    worked on tutorial 5&6, both would want someone to look over

--- tutorials/tutorial5.php
+++ tutorials/tutorial5.php
@@ -14,6 +14,142 @@
                 </p>
                 <button class="multipleChoice" onclick="window.location.href='../quizzes/quiz5.php';">Go to Quiz 5: UNIX and File Permissions</button>
             </div>
+			
+			<p>
+				In UNIX, security for users is paramount and ultimately, preventing data corruption and unsolicited access is successfully achieved through the use of UNIX’s ownership groups and permissions. 
+				Making sure your web files have proper permissions and ownerships can make or break your website's accessibility to your designated audience. 
+				Let's dive into the concept of file permissions to gain a better understanding of how we properly use permissions to our advantage!
+			</p>
+			
+			<div>
+				
+				<h2 id="files">
+					File Ownership 
+				</h2>
+				
+				<p>
+					Let's begin with the concept of ownership. Each and every file in UNIX is assigned three different ownership categories. 
+					These include: <span class="text-bold">user</span>, <span class="text-bold">group</span>, and <span class="text-bold">other</span>. The <span class="text-bold">user</span> group simply represents the owner of the file. By default, whoever creates the file becomes the owner. 
+					The user group is often denoted by a <span class="text-bold">(u)</span>. The category named <span class="text-bold">group</span> can hold multiple different types of users. 
+					With groups, all users belonging to the same group will have similar permissions when attempting to access a file. 
+					Instead of changing permissions for hundreds of individual users, adding them all to the same group and changing group permissions accordingly is an effective way to manage multiple user accounts. 
+					The group category is usually denoted by a <span class="text-bold">(g)</span>. Lasty, users belonging to the <span class="text-bold">other</span> group are typically thought of as “everyone else” trying to access a file, meaning, they didn’t create the file in question nor do they belong to a group who could own the file. 
+					The other group is often denoted using an <span class="text-bold">(o)</span>.
+				</p>
+				
+				<h2>
+					File Permissions
+				</h2>
+				
+				<p>
+					With the three ownership groups discussed above, being able to limit how these groups interact with files is where the concepts of permissions come into play. 
+					For each ownership group, UNIX assigns three different types of permissions: <span class="text-bold">read</span>, <span class="text-bold">write</span> and <span class="text-bold">execute</span>. 
+					<span class="text-bold">Read</span> permissions give the special authority to open, and thus read, a file. For a directory, this simply entails being able to list its contents. 
+					<span class="text-bold">Write</span> authorizes the modification of a file. On directories, the removal, addition, and or renaming of a file is granted. 
+					Lastly, having <span class="text-bold">execute</span> permissions refers to an ownership group being granted the ability to execute a file like a script or program. 
+					Like ownership categories, file permissions are sometimes denoted by specific characters with <span class="text-bold">read</span> permissions being denoted by the <span class="text-bold">(r)</span> character, <span class="text-bold">write</span> permissions being denoted by the <span class="text-bold">(w)</span> character, and <span class="text-bold">execute</span> permissions being denoted by the <span class="text-bold">(x)</span> character. 
+				</p>
+				
+				<h2>
+					File Ownership and Permissions Simplified
+				</h2>
+				
+				<img src="../assets/images/tutorial5/1.png" alt="File Ownership and Permissions" class="tutimg">
+				
+				<p>
+					There are three ownership types: <span class="text-bold">user(u)</span>, <span class="text-bold">group(g)</span>, and <span class="text-bold">other(o)</span>. Each ownership type contains three permissions: <span class="text-bold">read(r)</span>, <span class="text-bold">write(w)</span>, and <span class="text-bold">execute(x)</span>. 
+					Each file or directory in the UNIX filesystem is assigned all three ownership types, with each type containing fields for each file permission.
+				</p>
+				
+			</div>
+			
+			<div>
+				<h2 id="change">
+					Remember ls -l?
+				</h2>
+				
+				<p>
+					You may be asking yourself how you actually see ownership and permissions on files and directories. 
+					Well, do you remember the <span class="text-bold">ls -l</span> command? This command will show file permissions in the first column as well as a plethora of useful information to know about a file. 
+					While the words read, write and execute aren’t explicitly shown, they are abbreviated with <span class="text-bold">(r)</span>, <span class="text-bold">(w)</span>, and <span class="text-bold">(x)</span> respectively. In addition, the special <span class="text-bold">d</span> character is shown at the beginning of a file’s permission list to differentiate between files and directories. 
+					It’s also important to note that in some places, <span class="text-bold">a -</span> is shown. This suggests that there is no permission for that specific ownership group on a file. Let’s look at an example to demonstrate this.
+				</p>
+				
+				<img src="../assets/images/tutorial5/2_cropped.png" alt="List Ownership and Permissions" class="tutimg">
+				
+				<p>
+					As you can see, the above image lists the information for “file.txt”. Each colored box represents a different part of the files overall permissions.
+				</p>
+				
+				<h3>
+					Changing Permissions
+				</h3>
+				
+				<p>
+					Knowing all of this about permissions and ownership groups, being able to change certain permissions is a very important tool for UNIX users, ultimately giving them total control over who and what other users can do with their files. 
+					In order to change file permissions, we can use the <span class="text-bold">chmod</span> command. The chmod command has the following syntax: <span class="text-bold">chmod <permissions_to_add> <filename></span>. 
+					There are two ways to change file permissions: either through <span class="text-bold">absolute</span> or <span class="text-bold">symbolic</span> mode. In absolute mode, the permissions themselves are represented not by characters, but, by three digit, octal numbers. 
+					The following cheat sheet can help you understand the values associated with specific permission types.
+				</p>
+				
+				<img src="../assets/images/tutorial5/3.png" alt="Permission Types" class="tutimg">
+				
+				<p>
+					Let's look at some examples of chmod commands using absolute mode!
+				</p>
+				
+				<ul>
+					<h4 class="text-bold">chmod 755 file.txt</h4>					
+					<ol>Gives the user ownership group read, write, and execute permissions while both group and other ownership groups only get read and execute permissions for file.txt.</ol>
+					
+					<h4 class="text-bold">chmod 644 file.txt</h4>
+					<ol>Gives the user ownership group read and write permissions while both group and other ownership groups only get read permissions for file.txt.</ol>
+					
+					<h4 class="text-bold">chmod 744 file.txt</h4>
+					<ol>Gives the user ownership group read, write, and execute permissions while both group and other ownership groups only get read permissions for file.txt.</ol>
+				</ul>
+				
+				<p>
+					Contrastingly, with symbolic mode, you use mathematical symbols to modify file permissions. Using the <span class="text-bold">(+)</span> character adds a permission, using the <span class="text-bold">(-)</span> character removes a permission, and a <span class="text-bold">(=)</span> character sets permissions while overriding any earlier permissions. 
+					To target specific ownership groups, you can use <span class="text-bold">(u)</span>, <span class="text-bold">(g)</span>, <span class="text-bold">(o)</span>, and <span class="text-bold">(a)</span> for user, group, other, and all respectively. 
+				</p>
+				
+				<p>
+					Let's look at some examples of chmod commands using symbolic mode!
+				</p>
+				
+				<ul>
+					<h4 class="text-bold">chmod u+rw file.txt</h4>
+					<ol>Gives the user ownership group read and execute permissions for file.txt</ol>
+					
+					<h4 class="text-bold">chmod g-x file.txt</h4>
+					<ol>Removes execute permissions from group for file.txt</ol>
+					
+					<h4 class="text-bold">chmod o=rwx file.txt</h4>
+					<ol>Sets read, write, and execute permissions are assigned to others group for file.txt.</ol>
+					
+					<h4 class="text-bold">chmod a+rwx file.txt</h4>
+					<ol>Adds read, write, and execute permissions to user, group and other ownership groups for file.txt.</ol>
+				</ul>
+				
+				<h3>
+					Changing Owner and Group
+				</h3>
+				
+				<p>
+					As an ending note, if you ever wanted to change the direct owner of a file or group for a file, <span class="text-bold">chown</span> and <span class="text-bold">chgrp</span> will do the job. 
+					chown has the following syntax: <span class="text-bold">chown <new_user> <filename></span> where <span class="text-bold"><new_user></span> is the file's new owner. 
+					chgrp has a similar syntax with: <span class="text-bold">chgrp <new_group> <filename></span> where <span class="text-bold"><new_group></span> is the file's new owner group. 
+					In order to execute these commands, you may need to elevate your user privileges using the <span class="text-bold">sudo</span> command before the command call. 
+					For example, <span class="text-bold">sudo chown root file.txt</span>. Let’s look at an example
+				</p>
+				
+				<img src="../assets/images/tutorial5/4_cropped.png" alt="Changing Owner and Group" class="tutimg">
+				<p>
+					From the above example, we can see that after executing the both chown and chgrp commands, “file.txt” has a new user owner and group owner “root”. 
+					We have successfully changed the user and group owners for a file!
+				</p>
+			</div>
 
             <div>
                 <button class="multipleChoice centerButton" onclick="window.location.href='../quizzes/quiz5.php';">Go to Quiz 5: UNIX and File Permissions</button>
--- tutorials/tutorial6.php
+++ tutorials/tutorial6.php
@@ -14,7 +14,171 @@
                 </p>
                 <button class="multipleChoice" onclick="window.location.href='../quizzes/quiz6.php';">Go to Quiz 6: Helpful Commands</button>
             </div>
-
+			
+			<div>
+				<p>
+					Without a doubt, the best way to learn UNIX commands is through practice and experimentation. 
+					The commands and concepts you have already learned are a great starting point, but, as you dive deeper into UNIX and your web development journey, 
+					the following miscellaneous commands may be extremely helpful.
+				</p>
+				
+				<h2>
+					Clear
+				</h2>
+				
+				<p>
+					Executing commands in UNIX can quickly fill and clutter your terminal shell. An easy way to reset your terminal shell is through the clear command. 
+					Clear can easily remove the outputs of previous commands, bringing your terminal to its default state. Try it out for yourself!
+				</p>
+				
+				<img src="../assets/images/tutorial6/1_cropped.png" alt="Full Terminal Shell" class="tutimg">
+				<img src="../assets/images/tutorial6/2_cropped.png" alt="Cleared Terminal Shell" class="tutimg">
+				
+				<p>
+					Above are two terminals, the left showing a filled terminal with command outputs and the right showcasing a cleared terminal after using the clear command.
+				</p>
+				
+				<h2>
+					Double Up On Arrow Keys Shortcut
+				</h2>
+				
+				<p>
+					As you experiment with UNIX, using the same commands over and over will quickly become a reality. In order to save time, you can press the <span class="text-bold">up arrow key</span> “↑” to quickly cycle through commands you’ve already used. 
+					This shortcut is a great tool for recalling and easily modifying any previously used commands.
+				</p>
+				
+				<h2>
+					Tab Autocomplete
+				</h2>
+				
+				<p>
+					Another way in which you can drastically decrease the time spent typing commands is by using tab autocomplete. By using the <span class="text-bold">Tab</span> key, UNIX will automatically suggest completed command options for you. 
+					Tab autocomplete becomes extremely useful when having to deal with length file, path, or directory names. For example, let’s say in the “CobraTutorials” directory, we have two files, 
+					one named “This_filename_is_long.txt” and the other named “Long_this_filename_is.txt”. Instead of manually typing in these filenames, we could tab autocomplete them. 
+					I could press the <span class="text-bold">T</span> key and tab to automatically fill in “This_filename_is_long.txt” in my terminal or I could press the <span class="text-bold">L</span> key and tab to automatically fill in “Long_this_filename_is.txt” in my terminal. 
+					Next time you are writing commands, give tab autocomplete a try!
+				</p>
+				
+				<h2>
+					Touch Command
+				</h2>
+				
+				<p>
+					If you ever need to create a file in UNIX, the touch command can easily do this for you. The touch command has the following syntax: touch <filename> with filename being the name of a file you want to create. 
+					When using the touch command, any file you create will be empty by default. Changing a file's contents will be covered later in this tutorial. Let’s try creating a file using touch!
+				</p>
+				
+				<img src="../assets/images/tutorial6/3_cropped.png" alt="Touch Command" class="tutimg">
+				
+				<p>
+					In the above example, we use the touch command to create a file named “newfile.txt”. Again “newfile.txt” is empty by default but how can we check that this file is empty? 
+					That’s where the next command will become useful!
+				</p>
+				
+				<h2>
+					Cat Command
+				</h2>
+				
+				<p>
+					The cat, or concatenate command, allows a user to easily look inside files and read any data  to standard terminal output. 
+					Let’s say we wanted to check the contents of the file “newfile.txt” made from the previous exercise. We can do this using the following command syntax: cat <filename>. 
+					Let’s give this command a try!
+				</p>
+				
+				<img src="../assets/images/tutorial6/4_cropped.png" alt="Cat Command" class="tutimg">
+				
+				<p>
+					In the image above, we verified that the file “newfile.txt” was indeed empty since no data was read to standard output. How could we actually edit “newfile.txt” to contain some information? 
+					Well, we could use a terminal based text editor! Let’s jump to the next exercise to learn how to use some!
+				</p>
+			</div>
+			
+			<div>
+				<h2>
+					Nano and Vim Text Editors
+				</h2>
+				
+				<p>
+					Two popular UNIX text editors are Nano and Vim. Both editors are very different from one another; however, no matter which editor you use, they both come equipped with the proper tools to effectively edit files. 
+					To open a file in Nano, you use the command with the following syntax: nano <filename>. To open a file in Vim, you use the command with the following syntax: vim <filename>. If you use either of these commands without a filename or with a filename that does not yet exist, 
+					an empty file will be created in the editor of your choice. Let’s edit “newfile.txt” from the previous exercise to actually contain some information.
+				</p>
+				
+				<h3>
+					Editing With Nano
+				</h3>
+				
+				<p>
+					To open “newfile.txt” type in the following command: nano newfile.txt. You will be prompted with the following interface:
+				</p>
+				
+				<img src="../assets/images/tutorial6/5_cropped.png" alt="Open Nano Text File" class="tutimg">
+				
+				<p>
+					You can immediately begin to type Nano’s interface, editing the contents of “newfile.txt” as you see fit. 
+					Nano also provides the user with plenty of shortcut options at the bottom of the interface to help them edit their files. 
+					For our purposes, let’s edit “newfile.txt” to read “This file was edited with nano!”. When you want to save any changes you’ve made to the file, you can press Ctrl + o. 
+					This will prompt the user with the ability to change the filename if they so choose, but, after immediately pressing the Enter key, all changes will be saved to the current file.
+				</p>
+				
+				<img src="../assets/images/tutorial6/6_cropped.png" alt="Writing Nano Text File" class="tutimg">
+				<p>In this case, nano reads “Wrote 1 line” to suggest all changes to the file have been saved.</p>
+				
+				<p>
+					In order to exit nano, you can press Ctrl + x. Again, the user may be asked if they want to save any new changes made to the file. A user may press the Y key to save any changes or the N key to discard any changes. 
+					Let’s check to see if “newfile.txt” has any new contents!
+				</p>
+				<img src="../assets/images/tutorial6/7_cropped.png" alt="Reading Nano Text File Using Cat" class="tutimg">
+				<p>As you can see, after using the cat command, “newfile.txt” now outputs “This file was edited with nano!” to the command line. We have successfully edited a file using Nano!</p>
+				
+				<h3>
+					Editing With Vim
+				</h3>
+				
+				<p>
+					To open “newfile.txt”, use the following command: vim newfile.txt. You will be prompted with the following interface: 
+				</p>
+				<img src="../assets/images/tutorial6/8_cropped.png" alt="Opening newfile.txt Using Vim" class="tutimg">
+				
+				<p>
+					We can see that “newfile.txt” contains “This file was edited with nano!” from the nano example. One major difference between Vim and Nano is that unlike Nano, you cannot edit a file immediately in Vim. 
+					Instead, you must press the I key to enter “Insert” mode. The user can tell if they entered “Insert” mode if the button left of the interface reads “-- INSERT --”. 
+					Now that we can edit the information, let’s add the line “This file was edited with vim” underneath the previously added line.
+				</p>
+				
+				<img src="../assets/images/tutorial6/9_cropped.png" alt="Editing newfile.txt with Vim" class="tutimg">
+				<p>As you can see, we have entered “Insert” mode and we have edited “newfile.txt” to contain “This file was edited with vim!”. </p>
+				
+				<p>
+					Now that we have edited the file, we need to exit “Insert” mode. In order to do this, we can press the esc or escape key. After exiting “Insert” mode, we need to save our changes and exit the file. 
+					A quick way to do this is by entering :wq. This command will write all changes to the file and quit the Vim interface. If you ever wanted to leave Vim without saving any changes, you can enter :q! to exit. 
+					Let’s check to see if “newfile.txt” has any new contents!
+				</p>
+				<img src="../assets/images/tutorial6/10_cropped.png" alt="Reading newfile.txt Using Cat" class="tutimg">
+				
+				<p>
+					As you can see, after using the cat command, “newfile.txt” now outputs “This file was edited with nano!” and “This file was edited with vim!” to the command line. We have successfully edited a file using Vim!
+				</p>
+			</div>
+			
+			<div>
+				<h2>
+					Man Command
+				</h2>
+				
+				<p>
+					UNIX is full of hundreds of case-sensitive commands. Being able to remember what each command does and all the options associated with it each command is a tall order. 
+					Thankfully UNIX has a manual, or man, command. The manual command displays information regarding the functionality and purpose behind each UNIX command. It is an excellent tool to use if you ever forget what a command does. 
+					The syntax for this command is man <command_name>. Let’s look at the man page for the ls command using the following command: man ls.
+				</p>
+				<img src="../assets/images/tutorial6/11_cropped.png" alt="Using man ls Command" class="tutimg">
+				
+				<p>
+					This is just a small snippet of the manual for ls but, as you can see, the manual for ls describes how to use the command and what options are available to it. 
+					Never be afraid to check the man pages for a UNIX command!
+				</p>
+			</div>
+			
             <div>
                 <button class="multipleChoice centerButton" onclick="window.location.href='../quizzes/quiz6.php';">Go to Quiz 6: Helpful Commands</button>
             </div>
