0499f55217998f086cac81ea9233c3d2dc7fa461
NeoRewind <neohofstrom@gmail.com>
2021-12-01 20:36:46 -0500

    added references.php

---
+++ references.php
@@ -0,0 +1,24 @@
+<?php
+    $page = "./";
+    include $page . "assets/includes/header.php";
+?>      
+            <!-- Bryce Hofstrom/Group 3, ISTE-240, 2198-->
+            <title>Sources</title>
+            <div class="about-start">
+              <h1>Sources</h1>
+              <h4>Check out our inspirations for Cobra Tutorials!</h4>
+            </div>
+            <p><a href="http://www.ee.surrey.ac.uk/Teaching/Unix/unixintro.html">Department of Electrical and Electronic Engineering: UNIX Introduction</a></p>
+            <p><a href="https://www.tutorialspoint.com/unix/index.htm">Tutorials Point: UNIX/LINUX Tutorial</a></p>
+            <p><a href="https://www.cs.csub.edu/howto/unix_survival_guide.html">California State University Bakersfield: Basic UNIX Survival Guide</a></p>
+            <p><a href="https://www.guru99.com/file-permissions.html">Guru99: File Permissions in Linux/Unix: How to Read/Write & Change?</a></p>
+            <p><a href="https://docs.oracle.com/cd/E19253-01/817-5789/emjjp/index.html">Oracle: Kernel Overview</a></p>
+            <p><a href="https://en.wikipedia.org/wiki/UNIX_architecture#Kernel">Wikipedia: Unix architecture</a></p>
+            <p><a href="https://fsl.fmrib.ox.ac.uk/fslcourse/unix_intro/access.html">FSL: Accessing a Unix System</a></p>
+            <p><a href="https://www.tecmint.com/explanation-of-everything-is-a-file-and-types-of-files-in-linux/">TecMint: Explanation of “Everything is a File” and Types of Files in Linux</a></p>
+            <p><a href="https://www.pngegg.com/en/png-zwiix/download">PNG EGG: UNIX Penguin</a></p>
+        
+
+<?php
+    include $page . "/assets/includes/footer.php";
+?>
