c31f8db2c36f06607238623e43b680c08ff0d7e8
Christopher Bo <csb3946@g.rit.edu>
2021-12-02 08:01:04 -0500

    removed an unused code source, listed code and image sources on our references page

--- references.php
+++ references.php
@@ -8,17 +8,23 @@
               <h1><span class="color-black">Sources</span></h1>
               <h4>Check out our inspirations for Cobra Tutorials!</h4>
             </div>
-            <div>
-                <p class="srccontent"><a href="http://www.ee.surrey.ac.uk/Teaching/Unix/unixintro.html">Department of Electrical and Electronic Engineering: UNIX Introduction</a></p>
-                <p class="srccontent"><a href="https://www.tutorialspoint.com/unix/index.htm">Tutorials Point: UNIX/LINUX Tutorial</a></p>
-                <p class="srccontent"><a href="https://www.cs.csub.edu/howto/unix_survival_guide.html">California State University Bakersfield: Basic UNIX Survival Guide</a></p>
-                <p class="srccontent"><a href="https://www.guru99.com/file-permissions.html">Guru99: File Permissions in Linux/Unix: How to Read/Write & Change?</a></p>
-                <p class="srccontent"><a href="https://docs.oracle.com/cd/E19253-01/817-5789/emjjp/index.html">Oracle: Kernel Overview</a></p>
-                <p class="srccontent"><a href="https://en.wikipedia.org/wiki/UNIX_architecture#Kernel">Wikipedia: Unix architecture</a></p>
-                <p class="srccontent"><a href="https://fsl.fmrib.ox.ac.uk/fslcourse/unix_intro/access.html">FSL: Accessing a Unix System</a></p>
-                <p class="srccontent"><a href="https://www.tecmint.com/explanation-of-everything-is-a-file-and-types-of-files-in-linux/">TecMint: Explanation of “Everything is a File” and Types of Files in Linux</a></p>
-                <p class="srccontent"><a href="https://www.pngegg.com/en/png-zwiix/download">PNG EGG: UNIX Penguin</a></p>
-            </div>
+            <ol>
+                <li class="srccontent"><a href="http://www.ee.surrey.ac.uk/Teaching/Unix/unixintro.html">Department of Electrical and Electronic Engineering: UNIX Introduction</a></p>
+                <li class="srccontent"><a href="https://www.tutorialspoint.com/unix/index.htm">Tutorials Point: UNIX/LINUX Tutorial</a></p>
+                <li class="srccontent"><a href="https://www.cs.csub.edu/howto/unix_survival_guide.html">California State University Bakersfield: Basic UNIX Survival Guide</a></p>
+                <li class="srccontent"><a href="https://www.guru99.com/file-permissions.html">Guru99: File Permissions in Linux/Unix: How to Read/Write & Change?</a></p>
+                <li class="srccontent"><a href="https://docs.oracle.com/cd/E19253-01/817-5789/emjjp/index.html">Oracle: Kernel Overview</a></p>
+                <li class="srccontent"><a href="https://en.wikipedia.org/wiki/UNIX_architecture#Kernel">Wikipedia: Unix architecture</a></p>
+                <li class="srccontent"><a href="https://fsl.fmrib.ox.ac.uk/fslcourse/unix_intro/access.html">FSL: Accessing a Unix System</a></p>
+                <li class="srccontent"><a href="https://www.tecmint.com/explanation-of-everything-is-a-file-and-types-of-files-in-linux/">TecMint: Explanation of “Everything is a File” and Types of Files in Linux</a></p>
+                <li class="srccontent"><a href="https://www.pngegg.com/en/png-zwiix/download">PNG EGG: UNIX Penguin</a></p>
+                <li class="srccontent"><a href="https://youtu.be/9YffrCViTVk">Tipswithpunch: Embed a YouTube Video in HTML and Make it Responsive</a></p>
+                <li class="srccontent"><a href="https://www.flaticon.com/free-icon/witness_2210317?term=eye&page=1&position=16&page=1&position=16&related_id=2210317&origin=search#">The eye icon on our home page</a></p>
+                <li class="srccontent"><a href="https://www.flaticon.com/premium-icon/headphones_527989?term=headphones&page=1&position=1&page=1&position=1&related_id=527989&origin=search#">The headphones icon on our home page</a></p>
+                <li class="srccontent"><a href="https://www.flaticon.com/free-icon/check-mark_64484?term=checkmark&page=1&position=1&page=1&position=1&related_id=64484&origin=search#">The checkmark icon on our home page</a></p>
+                <li class="srccontent"><a href="https://unsplash.com/photos/IgUR1iX0mqM ">The splash image on our home page</a></p>
+                <li class="srccontent"><a href="https://www.w3schools.com/howto/tryit.asp?filename=tryhow_css_about_page ">W3Schools About Us TryIt Page</a></p>
+            </ol>
 
 <?php
     include $page . "/assets/includes/footer.php";
